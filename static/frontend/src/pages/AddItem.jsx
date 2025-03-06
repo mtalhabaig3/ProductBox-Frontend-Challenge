728f6d8a590b6c0f9c649e345a98918becf3b80b
Muhammad Talha Baig <mtalhabaig3@gmail.com>
2025-03-06 22:30:44 +0500

    implemented basic add item page
    implemented basic add item page which adds an item in total items

--- static/frontend/src/pages/AddItem.jsx
+++ static/frontend/src/pages/AddItem.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useAddItemMutation } from "../api/itemsApi";
+import { TextField, Button, Container } from "@mui/material";
 
 const AddItem = () => {
-  return <div>AddItem</div>;
+  const [addItem, { isLoading, isError }] = useAddItemMutation();
+
+  const formik = useFormik({
+    initialValues: { name: "", price: "", img: "" },
+    validationSchema: Yup.object({
+      name: Yup.string().required(),
+      price: Yup.number().required(),
+      img: Yup.string().url().required(),
+    }),
+    onSubmit: (values) => addItem(values),
+  });
+
+  return (
+    <Container>
+      <h2>Add New Item</h2>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField label="Name" {...formik.getFieldProps("name")} fullWidth />
+        <TextField
+          margin="normal"
+          label="Price"
+          type="number"
+          {...formik.getFieldProps("price")}
+          fullWidth
+        />
+        <TextField
+          margin="normal"
+          label="Image URL"
+          {...formik.getFieldProps("img")}
+          fullWidth
+        />
+        <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+          Add Item
+        </Button>
+      </form>
+    </Container>
+  );
 };
 
 export default AddItem;
