4b22c8840252ecaf3678065f253bdf9d6be68066
Muhammad Talha Baig <mtalhabaig3@gmail.com>
2025-03-08 00:28:08 +0500

    improved the design of additem.jsx

--- static/frontend/src/pages/AddItem.jsx
+++ static/frontend/src/pages/AddItem.jsx
@@ -1,7 +1,16 @@
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useAddItemMutation } from "../api/itemsApi";
-import { TextField, Button, Container } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Container,
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  InputAdornment,
+} from "@mui/material";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
@@ -36,42 +45,71 @@ const AddItem = () => {
   });
 
   return (
-    <Container>
-      <h2>Add New Item</h2>
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          label="Name"
-          {...formik.getFieldProps("name")}
-          fullWidth
-          error={formik.touched.name && Boolean(formik.errors.name)}
-          helperText={formik.touched.name && formik.errors.name}
-        />
-        <TextField
-          margin="normal"
-          label="Price"
-          type="number"
-          {...formik.getFieldProps("price")}
-          fullWidth
-          error={formik.touched.price && Boolean(formik.errors.price)}
-          helperText={formik.touched.price && formik.errors.price}
-        />
-        <TextField
-          margin="normal"
-          label="Image URL"
-          {...formik.getFieldProps("img")}
-          fullWidth
-          error={formik.touched.img && Boolean(formik.errors.img)}
-          helperText={formik.touched.img && formik.errors.img}
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          sx={{ mt: 2 }}
-          disabled={isLoading}
-        >
-          {isLoading ? "Adding..." : "Add Item"}
-        </Button>
-      </form>
+    <Container maxWidth="sm" sx={{ mt: 5 }}>
+      <Card sx={{ boxShadow: 3, borderRadius: 2 }}>
+        <CardContent>
+          <Typography
+            variant="h5"
+            fontWeight="bold"
+            gutterBottom
+            textAlign="center"
+          >
+            Add New Item
+          </Typography>
+
+          <form onSubmit={formik.handleSubmit}>
+            {/* Name Field */}
+            <TextField
+              label="Name"
+              {...formik.getFieldProps("name")}
+              fullWidth
+              margin="normal"
+              error={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+            />
+
+            {/* Price Field */}
+            <TextField
+              label="Price"
+              type="number"
+              {...formik.getFieldProps("price")}
+              fullWidth
+              margin="normal"
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && formik.errors.price}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+            />
+
+            {/* Image URL Field */}
+            <TextField
+              label="Image URL"
+              {...formik.getFieldProps("img")}
+              fullWidth
+              margin="normal"
+              error={formik.touched.img && Boolean(formik.errors.img)}
+              helperText={formik.touched.img && formik.errors.img}
+            />
+
+            {/* Submit Button */}
+            <Box textAlign="center" mt={3}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="large"
+                sx={{ px: 4, fontWeight: "bold" }}
+                disabled={isLoading}
+              >
+                {isLoading ? "Adding..." : "Add Item"}
+              </Button>
+            </Box>
+          </form>
+        </CardContent>
+      </Card>
     </Container>
   );
 };
